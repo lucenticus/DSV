f91654c7888a7c2dcd2f8a571eadf54e440e0412
Evgeny Pavlov <lucenticus@gmail.com>
2013-01-23 23:51:21 +0400

    Added key -DCONFIG_SCSI_OSD_DEBUG to preprocessing

--- run_analysis.sh
+++ run_analysis.sh
@@ -52,7 +52,7 @@ function preprocess_file
     afs_file="$work_dir/$work_file.afs"
     sem_file="$work_dir/$work_file.sem"
 
-    cc -E $orig_file -D__KERNEL__ -DMODULE -DCONFIG_SMP -DCONFIG_LOCK_KERNEL -I/usr/src/linux/include -I/usr/src/linux-headers/include/ -I/usr/src/linux-headers/arch/x86/include/ -I/usr/src/linux-headers-`uname -r`/include/ >$pp_file 2>$errors_file
+    cc -E $orig_file -D__KERNEL__ -DMODULE -DCONFIG_SMP -DCONFIG_LOCK_KERNEL -DCONFIG_SCSI_OSD_DEBUG -I/usr/src/linux/include -I/usr/src/linux-headers/include/ -I/usr/src/linux-headers/arch/x86/include/ -I/usr/src/linux-headers-`uname -r`/include/ >$pp_file 2>$errors_file
     sed -i~ "s/proc_handler *proc_handler/proc_handler *proc_h/g" $pp_file
     
     sed -i~ "s/pte_t ((pte_t) { (pteval_t val) } )/pte_t __dsv_fix_wrong_macros(pte_t val)/g" $pp_file
@@ -79,7 +79,7 @@ function preprocess_file
 	$DSV_DIR/afs2reqs $afs_file $sem_file >> $errors_file 2>&1
 	
 	if [ "$?" -ne "0" ]; then
-	    $DSV_DIR/afs2reqs $afs_file $sem_file --log>> $errors_file 2>&1
+	    #$DSV_DIR/afs2reqs $afs_file $sem_file --log>> $errors_file 2>&1
 	    echo "Can't analyze file: $afs_file" 
 	else
 	
