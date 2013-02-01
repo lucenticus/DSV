6426c572054c285890d1a8fc4c31d2cae960e7be
Evgeny Pavlov <lucenticus@gmail.com>
2013-02-01 15:08:44 +0400

    Added copyright

--- afs.h
+++ afs.h
@@ -1,3 +1,27 @@
+/*
+ * afs.h - functions for translating from C language to 
+ * Asynchronous Functional Schemes language
+ *
+ * Copyright (C) 2012 Evgeny Pavlov <lucenticus@gmail.com>
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This file is part of DSV.
+ *
+ * DSV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * DSV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with DSV.  If not, see <http://www.gnu.org/licenses/>.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
 #ifndef AFS_H
 #define AFS_H
 #include <stdio.h>
